3c15637ae03a6262f69177ac611c66ef5cf587ad
Ray Riney <rayfordriney@gmail.com>
2020-01-23 19:22:15 -0800

    first commit

---
+++ App/Data/friends.js
@@ -0,0 +1,17 @@
+var defaultFriends = [{
+  friendName: "Han Solo",
+  photoURL: "http://i.imgur.com/owmkI4r.jpg",
+  questionArray: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+},
+{
+  friendName: "Luke Skywalker",
+  photoURL: "http://imgur.com/gallery/3I2Pm2S.jpg",
+  questionArray: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5],
+},
+{
+  friendName: "R2D2",
+  photoURL: "http://imgur.com/gallery/EM724.jpg",
+  questionArray: [1, 2, 1, 2, 1, 2, 1, 2, 1, 2],
+}]
+
+module.exports = defaultFriends
